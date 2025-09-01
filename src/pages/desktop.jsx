b2a2af74856a696c7df05bce38237ed0d642cb45
joebakid <josephbawo@gmail.com>
2025-09-01 08:47:03 +0100

    file changed

--- src/pages/desktop.jsx
+++ src/pages/desktop.jsx
@@ -6,7 +6,7 @@ export default function Laptops() {
     <ProductGrid
       title="Desktop"
       pageSize={8}
-      sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vRdtheOlj7I5IVDC5X4oSZ2AIMRp5Q1zno8jr4GcA13_-0ImJt31mOtxdwx--daJUySuvKrwe0K8xNG/pub?gid=0&single=true&output=csv"
+      sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vRbJAPJ_vCpSy2BECqmClWuCuxViYz_ms9r_BinNogvCpxDGvV8MStrMU72UqATCZgRLMnBo-GhJE-c/pub?gid=0&single=true&output=csv"
       whatsAppNumber="+234 805 471 7837"
     />
   );
