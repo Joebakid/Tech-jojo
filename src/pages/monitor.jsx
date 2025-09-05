c644c54b82c12a78101b10f1db615a0730617c4f
joebakid <josephbawo@gmail.com>
2025-09-05 19:28:51 +0100

    monitor csv

--- src/pages/monitor.jsx
+++ src/pages/monitor.jsx
@@ -2,5 +2,5 @@ import data from "../data/tablets.json";
 import ProductGrid from "../components/ProductGrid";
 
 export default function Desktops() {
-  return <ProductGrid title="monitors" items={data} pageSize={8} />;
+  return <ProductGrid  sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5ft5ucwP62CICVLAdQ3mhbd_d-kVAADV-0smlETAwSyvo_4C4N8WF78P0ygmXd4QTLU8XmlTfFXUn/pub?output=csv" items={data} pageSize={8} />;
 }
