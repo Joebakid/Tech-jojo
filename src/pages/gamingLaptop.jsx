0e56f6d0e25127665ae758bcd9d25b6cecbcd222
joebakid <josephbawo@gmail.com>
2025-09-05 20:15:38 +0100

    typo

--- src/pages/gamingLaptop.jsx
+++ src/pages/gamingLaptop.jsx
@@ -4,7 +4,7 @@ import ProductGrid from "../components/ProductGrid";
 
 export default function Desktops() {
   return ( <ProductGrid
-        title="gaming Laptops"
+        title="gaming laptops"
         pageSize={8}
         sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vTYg3-LbibWCpecanR1TGzoNWUOnLAgbDVU1H_I0KsuXxoO444jKUt1P2LTYrxc_WkapaAMj6ozIgQ8/pub?output=csv"
         whatsAppNumber="+234 805 471 7837"
