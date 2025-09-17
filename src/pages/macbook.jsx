b51ff2adb32724b39399bb59caa64eda0efe3ba7
joebakid <josephbawo@gmail.com>
2025-09-18 00:30:42 +0100

    macbooks title corrected

--- src/pages/macbook.jsx
+++ src/pages/macbook.jsx
@@ -5,7 +5,7 @@ import MacBookFilters from "../components/filters/Macbookfilter";
 export default function MacBook() {
   return (
     <ProductGrid
-      title="macbook"
+      title="macbooks"
       // TODO: replace with your MacBook CSV publish link
       sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vQxuYThDUuYKmEwO5b1c30ym4Z7ZF8ID528VHQ97lQm1HdZxsAXmVdJpoGmOcYXvO4IptOyNmWsOsXF/pub?output=csv"
       pageSize={8}
