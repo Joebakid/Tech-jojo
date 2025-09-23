e747cf569cf74a5c6421e5210809020d9186b0f6
joebakid <josephbawo@gmail.com>
2025-09-23 19:16:40 +0100

    csv link changed

--- src/pages/desktop.jsx
+++ src/pages/desktop.jsx
@@ -6,7 +6,7 @@ export default function Desktops() {
   return (
     <ProductGrid
       title="desktops"
-      sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vT7RwRTOVgcoRrMtp6WhEzQDgjtO3aOX2HkzRl7rJm5zb6JIX0K1LPIsB8t-ASRKUUyc7Rp5W9G6iOf/pub?gid=475590999&single=true&output=csv"
+      sheetCsvUrl="https://docs.google.com/spreadsheets/d/e/2PACX-1vSPuCdo6sGWyH86QZWQLNZLA7Ybd4x_KoxBLLpo0qdZjAlgkvuunJaP8hp_ELQHy5sT_4BG61C0SrIu/pub?output=csv"
       pageSize={8}
       whatsAppNumber="+234 805 471 7837"
       renderFilters={DesktopFilters}
